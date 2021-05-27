87e3801266723018cc536f5c5ae9afb9ba24529f
Surabhi G <42394955+Surabhi102@users.noreply.github.com>
2021-05-27 22:44:37 +0530

    Add files via upload
    Updated portfolio

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$('.port-item').click(function () {
+    $('.collapse').collapse('hide');
+  });
+
+  var typed = new Typed(".typing", {
+      strings: ["Developer", "Teacher"],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+  });
